14936a66c0bddd4e8fc67dcf517de7e6c7bec27a
Darius <Darius@gmail.com>
2025-01-17 12:40:39 +0200

    vartotojo autorizacija serveryje

--- virtual_bank/server/index.js
+++ virtual_bank/server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
+import session from 'express-session';
+import users from './controller/users.js';
 
 const app = express();
 
@@ -15,7 +17,25 @@ const app = express();
 //     res.json('Hello');
 // });
 
+
+// konfiguracinė eilutė norint gauti duomenis JSON formatu
+app.use(express.json());
+
 await mongoose.connect('mongodb://127.0.0.1:27017/virtualBank');
+
+// autoriazacijos pradzia
+
+// Nurodymas, jog priimsi slapuko duomenis per proxy
+app.set('trust proxy', 1) 
+// Sesijos konfiguracija:
+app.use(session({
+  secret: 'Trys plius balta', // Slapta unikali frazė
+  resave: false, // Leidžiame slapuko perrašymą
+  saveUninitialized: true, // Išduodame sausainėlį nepriskyrus jokių reikšmių
+  cookie: { secure: false } // Secure nurodo ar kreipsimės per HTTPS protokolą
+}));
+
+app.use('/api', users);
 
 const Accounts = mongoose.model('Accounts', {
     firstName: String,
@@ -26,8 +46,7 @@ const Accounts = mongoose.model('Accounts', {
     balance: Number
 });
 
-// konfiguracinė eilutė norint gauti duomenis JSON formatu
-app.use(express.json());
+
 
 // funkcija atsitiktinių skaičių generavimui
 function random(min, max) {
